727839de3d0d75630328578e51f3ed9c0770daa8
AlexanderHD27 <alexander.wersching@t-online.de>
2022-01-04 13:42:33 +0100

    Started Building Assembler; Need an clear schceam for encoding instructions

--- tools/software-tool-chain/test_program.asm
+++ tools/software-tool-chain/test_program.asm
@@ -1,6 +1,13 @@
 
+#define X 5
+#include some_raw_data.asm
+hello_world_string:
+    db "Let's print \"Hello World\""
+    db 0xff 0x00 0xff 0x00
+
 ; Multiplies 2 with 3
 mov A '\t'
+add A A Y
 mov B 0x0003
 mov C ZERO
 
